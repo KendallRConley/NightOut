27fb46a05a029d0a69d006ef0c1131ddff8f3bde
KendallRConley <kendall.conley@uky.edu>
2019-04-08 14:09:56 -0400

    Made downloadHandler an inner class of pageResult, and made result push to UI.

--- app/src/main/java/com/example/night_out/pageResult.java
+++ app/src/main/java/com/example/night_out/pageResult.java
@@ -1,19 +1,21 @@
 package com.example.night_out;
-
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import okhttp3.*;
 
 public class pageResult extends AppCompatActivity {
-    TextView yelpQuery;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_result);
-        yelpQuery = findViewById(R.id.yelpQuery);
         Request request = new Request.Builder()
                 .url("https://api.yelp.com/v3/businesses/search?categories=seafood&price=1&location=Lexington,%20KY")
                 .get()
@@ -22,7 +24,35 @@ public class pageResult extends AppCompatActivity {
                 .addHeader("cache-control", "no-cache")
                 .addHeader("Postman-Token", "fb69fb24-4846-4a93-9e4e-1199fa4693ff")
                 .build();
-        AsyncTask<Request, Void, String> query = new downloadHandler().execute(request);
-        yelpQuery.setText("TESTING QUERY");
+        new downloadHandler().execute(request);
+    }
+
+    private class downloadHandler extends AsyncTask<Request, JSONObject, JSONObject> {
+        OkHttpClient client = new OkHttpClient();
+        @Override
+        protected JSONObject doInBackground(Request... requests) {
+            try {
+                Response response = client.newCall(requests[0]).execute();
+                JSONObject jsonObject = new JSONObject(response.body().string().trim());       // parser
+                JSONArray myResponse = (JSONArray)jsonObject.get("businesses");
+                return (myResponse.getJSONObject(0));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(JSONObject result){
+            TextView txt = findViewById(R.id.yelpQuery);
+            try {
+                txt.setText(result.getString("name"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
