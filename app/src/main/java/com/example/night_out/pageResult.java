2cd0be972b5dcc281f56f6a2c4a96b4a4fd58cce
KendallRConley <kendall.conley@uky.edu>
2019-04-08 19:34:40 -0400

    Printed more info about location to screen

--- app/src/main/java/com/example/night_out/pageResult.java
+++ app/src/main/java/com/example/night_out/pageResult.java
@@ -9,6 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Random;
 
 import okhttp3.*;
 
@@ -17,7 +18,7 @@ public class pageResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_result);
         Request request = new Request.Builder()
-                .url("https://api.yelp.com/v3/businesses/search?categories=seafood&price=1&location=Lexington,%20KY")
+                .url("https://api.yelp.com/v3/businesses/search?categories=seafood&price=1&latitude=38.047989&longitude=-84.501640")
                 .get()
                 .addHeader("Authorization", "Bearer C7UvUlP5eSCdaKyC0T5mjE8EXnhPm4lu_lGFDFAI_vzIzUkNNbqyl6pnaZMYFcU4p2E-93JFKP20URmNfBbc12sKJ-lugBGY6FxyCRFUFjMuBFmc9n3gHCdYq4KSXHYx")
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
@@ -35,7 +36,9 @@ public class pageResult extends AppCompatActivity {
                 Response response = client.newCall(requests[0]).execute();
                 JSONObject jsonObject = new JSONObject(response.body().string().trim());       // parser
                 JSONArray myResponse = (JSONArray)jsonObject.get("businesses");
-                return (myResponse.getJSONObject(0));
+                Random rand = new Random();
+                int objNum = rand.nextInt(myResponse.length());
+                return (myResponse.getJSONObject(objNum));
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
@@ -46,9 +49,15 @@ public class pageResult extends AppCompatActivity {
         }
         @Override
         protected void onPostExecute(JSONObject result){
-            TextView txt = findViewById(R.id.yelpQuery);
+            TextView name = findViewById(R.id.yelpName);
+            TextView add = findViewById(R.id.yelpAdd);
+            TextView phone = findViewById(R.id.yelpPhone);
+            TextView url = findViewById(R.id.yelpURL);
             try {
-                txt.setText(result.getString("name"));
+                name.setText(result.getString("name"));
+                add.setText(result.getString("location"));
+                phone.setText(result.getString("phone"));
+                url.setText(result.getString("url"));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
