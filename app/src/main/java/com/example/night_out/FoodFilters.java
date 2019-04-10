35cf094c86e2439d9b63c78641e5a2bedae5d2ec
Adrian <adrian.carideo@uky.edu>
2019-04-09 23:15:11 -0400

    Began prep for dynamic spinners once yelp api is implemented

--- app/src/main/java/com/example/night_out/FoodFilters.java
+++ app/src/main/java/com/example/night_out/FoodFilters.java
@@ -78,6 +78,7 @@ public class FoodFilters extends AppCompatActivity {
         });
         Spinner spinner_foodType = findViewById(R.id.spinner_foodType);
         //create arrayAdapter from the string resource for a spinner
+        //TODO on yelp api implementation, change to a new ArrayAdapter<String> w/ ArrayList>String> arg
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.food_types_array, android.R.layout.simple_spinner_item);
         //set the spinner layout in the adapter
